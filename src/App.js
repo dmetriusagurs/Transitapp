b7319b1f58e2e1bc6a3e8ae50a426c30c492d2fd
the antichrist <dmetrius.agurs@gmail.com>
2018-08-11 15:36:42 -0400

    CSS edits and Init Logo removed.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Axios from 'axios';
 import xml2js from 'xml2js';
@@ -26,9 +25,10 @@ class App extends Component {
   Axios.get(`${baseUrl}bustime/api/v3/gettime?key=${apiKey}`)
   .then(res =>
   {
-
+//this was made to remove the dash in bustime-response
     let xml = res.data.body.split('-').join('');
     console.log(xml);
+//parsestring for the XML syntax.
     xml2js.parseString(xml, {trim: true}, function (err, result) {
     console.log(result.bustimeresponse.tm);
     
@@ -44,13 +44,31 @@ class App extends Component {
 
  render() {
     return (
-      <div className="App">
-       'something';
+  <div className="title"> <h1>Pittsburgh Transit Ep: 6 - The Return of The Brown Line</h1>   
+  
+    <div className="text">
+      <p> The "Brown Line" (we didn't name it) was discontinued from service in 2010. It provided speedy light-rail access to Downtown from Allentown, a part of the city that is currently in the midst of a renovation.
 
+                Earlier this month, a train derailed, spilling Listerine (among other things) on the South Side Junction stop of the T, which required a work-around to carry public transit riders between Downtown and the South Hills.
 
-      </div>
-    );
-  }
+                Now, as people go about their day, in a largely transit-ignored part of the city, T's full of people have been zooming through the neighborhood, without stopping... and that led us to wonder why?
+
+                  With the North Side extension to the T, and the many entrepreneurial efforts that have sprouted up in Allentown, does it make sense to still not provide service to this neighborhood? Maybe, while the transit is re-routed, this can be the perfect opportunity to renew some service to this latent jewel in the crown of Pittsburgh!  
+
+                  Just adding ONE stop at Arlington and E Warrington would provide easy public transit to such exciting businesses as:
+           </p>
+              </div>
+<div className="Links">
+Black Forge Coffee  - <a href=" http://www.blackforgecoffee.com/">Black Forge Coffee </a> 
+Alla Famiglia  -  http://allafamiglia.com/
+Work Hard Pittsburgh  -  https://workhardpgh.com/
+Leon's Caribbean   -   http://www.leonscaribbean.com/
+Onion Maiden   -    http://www.onionmaiden.com/ 
+Weeping Glass    -    http://www.moroseandmacabre.com/  
+Super Monkey Recording Studio    -    http://www.ilovesupermonkey.com/ 
+</div>  </div>
+);  
+          }
 }
 
 export default App;
